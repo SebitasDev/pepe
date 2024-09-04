2208115249e962f7c5791686e08c2c5eeb7c9d72
Fernando José Gómez Toro <fjgt2000@gmail.com>
2024-09-04 16:14:31 -0500

    feat: se agrega funcionalidad de actualizar grupos

--- App/Controllers/Groups/GroupUpdateController.cs
+++ App/Controllers/Groups/GroupUpdateController.cs
@@ -1,37 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using RiwiTalent.Services.Interface;
 using RiwiTalent.Models;
 
 namespace RiwiTalent.App.Controllers.Groups
 {
-    public class GroupCreateController : Controller
+    public class GroupUpdateController : Controller
     {
         private readonly IGroupCoderRepository _groupRepository;
         public string Error = "Server Error: The request has not been resolve";
-        public GroupCreateController(IGroupCoderRepository groupRepository)
+        public GroupUpdateController(IGroupCoderRepository groupRepository)
         {
             _groupRepository = groupRepository;
         }
 
-        //endpoint
-        [HttpPost]
-        [Route("RiwiTalent/CreateGroups")]
-        public IActionResult Post([FromBody] GruopCoder gruopCoder)
+        //Endpoint
+        [HttpPut]
+        [Route("RiwiTalent/UpdateGroups")]
+        public async Task<IActionResult> UpdateGroups(GruopCoder gruopCoder)
         {
             if(!ModelState.IsValid)
             {
                 return BadRequest(Utils.Exceptions.StatusError.CreateBadRequest());
             }
-
+            
             try
             {
-                _groupRepository.add(gruopCoder);
-                return Ok("The Group has been created successfully");
+                await _groupRepository.Update(gruopCoder);
+                return Ok("The Group has been updated the correct way");
             }
             catch (Exception ex)
             {
                 throw new Exception(Error, ex);
             }
+
         }
     }
 }
